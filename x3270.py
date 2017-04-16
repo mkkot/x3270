5e6c941341dab5b73e0da820d80f687cdf3587e0
mk <mk@oc1212611155.ibm.com>
2017-04-16 15:17:43 +0200

    License and Readme added

--- x3270.py
+++ x3270.py
@@ -1,3 +1,7 @@
+# This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License.
+# https://creativecommons.org/licenses/by-sa/4.0/
+# Created by Marcin Kocur <marcin2006 AT gmail DOT com>, 2017
+
 #!/usr/bin/python
 
 import subprocess
